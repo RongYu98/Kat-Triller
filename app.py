690823f36d6dec803d3ef8b6a3ec215f3bf6dfe5
RongYu98 <ubuntu@hw0.cloud.compas.cs.stonybrook.edu>
2019-10-20 01:33:32 +0000

    feat: mongodb

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+# One database
+client = MongoClient("mongodb://localhost:27017/")
+db = client["kt_db"]
+# tablename = client['table_name'] 
+# https://www.w3schools.com/python/python_mongodb_create_collection.asp
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
