3eb64b87bb0a290e6628739eea31cbbbc0b8827f
rongyu98 <stuy.rong.yu@gmail.com>
2019-12-02 20:09:38 +0000

    fix: load testing from different VM

--- load_tester.py
+++ load_tester.py
@@ -34,8 +34,12 @@ class UserBehavior(TaskSet):
         
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    wait_time = between(2.0, 3.0) # how long to wait between requests
+    wait_time = between(1.0, 2.0) # how long to wait between requests
     
 
 # locust -f load_tester.py --no-web --run-time 1m -c 1000 -r 5000 --host http://localhost
 # Command to run the load_tester file, with no web ui, with 1090 concurrent users, at a hatch rate of 500/s
+
+
+# locust -f load_tester.py -c 1000 -r 5000 --host http://130.245.168.47
+# Command to run load_tester file, on another VM, from the web interface, port 8089
